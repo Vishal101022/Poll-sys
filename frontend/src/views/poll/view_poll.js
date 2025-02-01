a01450439f632dda9a4331f4c326e89b4133c750
Vishal101022 <raivishal939@gmail.com>
2025-02-01 23:32:55 +0530

    Remove unused SVG and styling from poll view component

--- frontend/src/views/poll/view_poll.js
+++ frontend/src/views/poll/view_poll.js
@@ -400,28 +400,7 @@ const ViewPoll = () => {
                 </div>
               </form>
             </div>
-            <div
-              className="flex justify-center items-center mt-5 text-gray-100"
-              style={{
-                color:
-                  poll.selected_theme?.colors?.pollQuestionColor || "#FFFFFF", // Fallback color
-              }}
-            >
-              <svg
-                className="h-4 w-4 -ml-1 mr-2 "
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                stroke=""
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </div>
-
+          
             {poll.allow_comments && <Comment pollId={pollId} />}
           </>
         )}
