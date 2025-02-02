bcf33234df187efd08cdd9d07be3225516f8e2cf
Vishal101022 <raivishal939@gmail.com>
2025-02-03 04:18:15 +0530

    Add profilePicture field to creator model for storing profile images

--- backend/src/models/creator.model.js
+++ backend/src/models/creator.model.js
@@ -9,11 +9,19 @@ const customSchema = new mongoose.Schema(
     totalPollsCreated: { type: Number, default: 0 },
     isCreator: { type: Boolean, default: false },
     isVoted: { type: Boolean, default: false },
+    profilePicture: {
+      type: String,
+      required: false,
+      default: null,
+    },
+
     verificationToken: { type: String },
     resetPasswordToken: { type: String },
     resetPasswordTokenExpiration: { type: Date },
     createdThemes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Theme" }],
-    createdTemplates: [{ type: mongoose.Schema.Types.ObjectId, ref: "Emailtemplate" }],
+    createdTemplates: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "Emailtemplate" },
+    ],
   },
   {
     versionKey: false,
