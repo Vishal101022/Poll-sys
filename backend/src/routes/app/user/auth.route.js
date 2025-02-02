0091200fda169b3b87216f34d401051eecc07c7e
Vishal101022 <raivishal939@gmail.com>
2025-02-03 04:18:41 +0530

    Add upload route for profile picture with authorization and handling

--- backend/src/routes/app/user/auth.route.js
+++ backend/src/routes/app/user/auth.route.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
 const authCtrl = require("../../../controllers/app/users/auth.controller.js");
+const authorization = require("../../../middleware/require-auth.js");
+const upload = require("../../../../config/multer.js");
+const uploadCtrl = require("../../../controllers/auth.controller.js");
 const router = express.Router();
 const { resMsg, resStatusCode } = require("../../../../config/constant.js");
 const { createResponse, createError } = require("../../../../utils/helpers.js");
 module.exports = router;
+
+/**
+ * @route Post api/v1/upload
+ * @description upload
+ * @returns JSON
+ * @access public
+ */
+router.post(
+  "/upload/",
+  authorization,
+  upload.single("profile_picture"),
+  asyncHandler(uploadProfile)
+);
 
 /**
  * @route GET api/v1/auth/register
@@ -43,9 +59,17 @@ router.post("/login", asyncHandler(login));
 async function register(req, res, next) {
   try {
     let response = await authCtrl.register(req);
-    if (response) return createResponse(res, resStatusCode.CREATED, resMsg.CREATED, response);
+    if (response)
+      return createResponse(
+        res,
+        resStatusCode.CREATED,
+        resMsg.CREATED,
+        response
+      );
     else
-      return createError(res, resStatusCode.UNABLE_CREATE, { message: resMsg.UNABLE_CREATE });
+      return createError(res, resStatusCode.UNABLE_CREATE, {
+        message: resMsg.UNABLE_CREATE,
+      });
   } catch (e) {
     return createError(res, resStatusCode.BAD_REQUEST, e);
   }
@@ -57,9 +81,17 @@ async function register(req, res, next) {
 async function verifyToken(req, res, next) {
   try {
     let response = await authCtrl.verifyToken(req);
-    if (response) return createResponse(res, resStatusCode.CREATED, resMsg.CREATED, response);
+    if (response)
+      return createResponse(
+        res,
+        resStatusCode.CREATED,
+        resMsg.CREATED,
+        response
+      );
     else
-      return createError(res, resStatusCode.UNABLE_CREATE, { message: resMsg.UNABLE_CREATE });
+      return createError(res, resStatusCode.UNABLE_CREATE, {
+        message: resMsg.UNABLE_CREATE,
+      });
   } catch (e) {
     return createError(res, resStatusCode.BAD_REQUEST, e);
   }
@@ -71,11 +103,25 @@ async function verifyToken(req, res, next) {
 async function login(req, res, next) {
   try {
     let response = await authCtrl.login(req);
-    if (response) return createResponse(res, resStatusCode.LOGIN, resMsg.LOGIN, response);
+    if (response)
+      return createResponse(res, resStatusCode.LOGIN, resMsg.LOGIN, response);
     else
-      return createError(res, resStatusCode.BAD_REQUEST, { message: resMsg.BAD_REQUEST });
+      return createError(res, resStatusCode.BAD_REQUEST, {
+        message: resMsg.BAD_REQUEST,
+      });
   } catch (e) {
     return createError(res, resStatusCode.BAD_REQUEST, e);
   }
 }
 
+async function uploadProfile(req, res) {
+  try {
+    const response = await uploadCtrl.uploadProfilePicture(req, res);
+    return createResponse(res, resStatusCode.CREATED, resMsg.CREATED, response);
+  } catch (e) {
+    console.error("Error in uploadProfile:", e);
+    return createError(res, resStatusCode.BAD_REQUEST, {
+      message: e.message || "Server error",
+    });
+  }
+}
